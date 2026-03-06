2a17428badebcc0a6234d3f884ca0d9576349bf2
VINOTH KUMAR S <53818245+vinothkumarbscit46@users.noreply.github.com>
2026-03-06 17:15:57 +0530

    fetch-movie-list

--- src/Pages/MovieList.jsx
+++ src/Pages/MovieList.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
 import Card from "../components/Card";
 
 const MovieList = () => {
+   
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjM1MDc4MjNhYWQyZjE0N2I4ZjM5NGM0YTFmYzc2MyIsIm5iZiI6MTcyNTcyMzI0MC43ODU5OTk4LCJzdWIiOiI2NmRjNzI2OGIxNWFhZDc4MDdiOTIxYjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZWISgEcrG37TuShn25KVQJgFWT4ZNFaPKNavC0KKqvk'
+  }
+  };
+
+  const [movies,setMovies] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchMovies(){
+
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing",options);
+
+      const data = await response.json();
+
+      setMovies(data.results);     
+
+    }
+
+    fetchMovies();
+  },[]);
+  
+  
   return (
     <main>
       <section className="max-w-7xl max-auto py-7">
         <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
-          <Card/>
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie}/>
+          ))
+          }
         </div>
       </section>
     </main>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,46 +1,31 @@
-const Card = () => {
+import { Link } from "react-router-dom";
+
+
+const Card = ({movie}) => {
+  
+  const {id, original_title, overview, poster_path} = movie; 
+  const image = `https://image.tmdb.org/t/p/w500/${poster_path}`
+  
+  
   return (
+    <Link to={`movie/${id}`}>
     <div className="bg-neutral-primary-soft mx-auto block max-w-xs p-6 border border-default rounded-base shadow-xs">
-            <a href="#">
-              <img
-                className="rounded-t-lg h-56 w-full object-cover"
-                src="https://images.unsplash.com/photo-1588862081126-293dc587ec51?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDMzfHx8ZW58MHx8fHx8"
-                alt=""
-              />
-            </a>
-            <a href="#">
-              <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">
-                Batman VI
-              </h5>
-            </a>
-            <p className="mb-6 text-body">
-              In today’s fast-paced digital landscape, fostering seamless
-              collaboration among Developers and IT Operations.
-            </p>
-            <a
-              href="#"
-              className="inline-flex items-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
-            >
-              Read more
-              <svg
-                className="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 12H5m14 0-4 4m4-4-4-4"
-                />
-              </svg>
-            </a>
-          </div>
+      <img
+        className="rounded-t-lg"
+        src={image}
+        alt=""
+      />
+
+            
+      <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">
+        {original_title}
+       </h5>
+      <p className="mb-6 text-body line-clamp-3">
+         {overview}
+      </p>
+            
+      </div>
+  </Link>
   )
 }
 
