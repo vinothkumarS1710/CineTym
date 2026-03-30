e6b8db261419bd779eb4944f02b34fae197d9ee2
VINOTH KUMAR S <53818245+vinothkumarbscit46@users.noreply.github.com>
2026-03-30 22:56:38 +0530

    addCardIntraction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const Footer = () => {
   return (
     <footer className="bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4">
-    <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-col items-center text-center md:flex-row md:justify-between md:text-left">
+    <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-col items-center text-center md:flex-row md:justify-center md:text-left">
         
         <span className="text-sm text-body">
         © 2026 
@@ -11,20 +11,7 @@ const Footer = () => {
         All Rights Reserved.
         </span>
 
-        <ul className="flex flex-wrap justify-center gap-4 mt-3 text-sm font-medium text-body md:mt-0">
-        <li>
-            <NavLink to="#" className="hover:underline">About</NavLink>
-        </li>
-        <li>
-            <NavLink to="https://www.youtube.com" className="hover:underline">Youtube</NavLink>
-        </li>
-        <li>
-            <NavLink to="https://www.instagram.com" className="hover:underline">Instagram</NavLink>
-        </li>
-        <li>
-            <NavLink to="https://x.com" className="hover:underline">X</NavLink>
-        </li>
-        </ul>
+        
 
     </div>
     </footer>
