a701703c7ed20d97bbae0e91b0ee107d3856db9c
VINOTH KUMAR S <53818245+vinothkumarbscit46@users.noreply.github.com>
2026-03-31 10:17:06 +0530

    setTheme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ const getIndicatorPosition = () => {
   }
 };
 
-  const activeClass = "block cursor-pointer w-24 px-3 py-1.5 text-center font-semibold text-[#ff3e03] rounded-xl md:text-[#ff3e03]";
+  const activeClass = "block cursor-pointer w-24 px-3 py-1.5 text-center font-semibold text-[#FF4B33] rounded-xl md:text-[#FF4B33]";
   const inActiveClass = "block cursor-pointer w-24 px-3 py-1.5 text-center font-semibold text-heading md:border-0";
 
   const handleSubmit = (e) => {
@@ -38,7 +38,7 @@ const getIndicatorPosition = () => {
   };
 
   return (
-    <header className="bg-neutral-primary w-full border-b border-[#ff3e03] md:border-[#ff3e03]">
+    <header className="bg-neutral-primary w-full border-b border-black md:border-black">
       <div className="mx-auto px-4 py-5 max-w-screen-xl">
 
         <div className="flex flex-col md:flex-row md:items-center md:justify-between">
@@ -56,10 +56,10 @@ const getIndicatorPosition = () => {
 
             <button
               onClick={() => setIsOpen(!isOpen)}
-              className="md:hidden p-2 w-10 h-10 flex items-center justify-center hover:bg-neutral-secondary-soft hover:text-heading focus:text-[#ff3e03]">
+              className="md:hidden p-2 w-10 h-10 flex items-center justify-center hover:bg-neutral-secondary-soft hover:text-heading focus:text-[#FF4B33]">
               <span className="sr-only">Open Main menu</span>
               <svg
-                className="w-6 h-6 text-[#ff3e03] rounded-3xl"
+                className="w-6 h-6 text-[#FF4B33] rounded-3xl"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -74,7 +74,7 @@ const getIndicatorPosition = () => {
           <div className={`flex flex-col md:flex-row md:items-center items-center md:space-x-6 w-full md:w-auto h-auto ${isOpen ? "block" : "hidden md:flex"} md:mt-0`}>
 
             <ul className="relative flex flex-col md:flex-row md:space-x-4 mt-8 mb-3 items-center w-56 md:w-auto md:mt-0 md:mb-0 border border-[#ff5703] md:border-0 rounded-2xl bg-neutral-secondary-soft md:bg-neutral-primary p-4 md:p-0">
-              <span className={` hidden sm:block absolute bottom-0 h-[2px] w-24 bg-gradient-to-r from-white via-[#ff3e03] to-white transition-all duration-300 ${getIndicatorPosition()}`}/>
+              <span className={` hidden sm:block absolute bottom-0 h-[2px] w-24 bg-gradient-to-r from-white via-[#FF4B33] to-white transition-all duration-300 ${getIndicatorPosition()}`}/>
               <li>
                 <NavLink to="/" end className={({ isActive }) => isActive ? activeClass : inActiveClass}>
                   Home
@@ -116,7 +116,7 @@ const getIndicatorPosition = () => {
                   type="text"
                   name="search"
                   placeholder="Search a movie"
-                  className="block w-full px-3 py-1.5 ps-9 pe-3 rounded-xl border border-black focus:ring-2 focus:ring-[#ff3e03] focus:border-none focus:outline-none rounded-2xl p-1 shadow-xs placeholder:text-body"
+                  className="block w-full px-3 py-1.5 ps-9 pe-3 rounded-xl border border-black focus:ring-2 focus:ring-[#FF4B33] focus:border-none focus:outline-none rounded-2xl p-1 shadow-xs placeholder:text-body"
                 />
               </div>
             </form>
